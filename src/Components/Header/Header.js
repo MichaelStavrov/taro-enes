c3684e8e49673f70ac93232dcce8e7d2218435cd
Michael <mihali5@mail.ru>
2021-06-11 15:06:09 +0300

    change photos

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -33,24 +33,26 @@ export function Header() {
             <div className={s.iconLogoWrapper}>
               <img className={s.iconLogo} src={iconLogo} alt="logo" />
             </div>
-            <h1 className={s.titleLogo}>Таро Энес</h1>
+            <div className={s.titleLogo}>Таро Энес</div>
           </Link>
           {showMenu && <div className={s.substrate} />}
           <button className={s.btnBurgerMenu} onClick={handleBtnMenuClick}>
             <span></span>
           </button>
-          
+
           <nav
             className={cn(s.navigation, {
               [s.vidgetNav]: showMenu,
             })}
           >
-          <button className={s.btnClose}></button>
+            <button className={s.btnClose}></button>
             <ul className={s.listNav}>
               {sectionTitles.map(({ id, name, path }) => (
-                <Link to={`/${path}`} className={s.itemNav} key={id}>
-                  {name.toUpperCase()}
-                </Link>
+                <li className={s.itemNav} key={id}>
+                  <Link to={`/${path}`} className={s.linkNav} >
+                    {name.toUpperCase()}
+                  </Link>
+                </li>
               ))}
             </ul>
           </nav>
--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -12,7 +12,7 @@ export function MainPage() {
       <Helmet>
         <meta
           name="description"
-          content="Расклады на все случаи жизни. Первый вопрос бесплатно!"
+          content="Расклады на все случаи жизни. Первый вопрос бесплатно! Авторская методика раслкадов. Расклады онлайн. Узнайте вероятности событий и необходимость на них повлиять."
         />
         <meta
           name="keywords"
@@ -86,78 +86,6 @@ export function MainPage() {
               </div>
               Instagram
             </a>
-            {/* <div className={s.col}>
-            <div className={s.imageOnMainPageWrapper}>
-              <img
-                className={s.imageOnMainPage}
-                src={imageOnMainPage}
-                alt="imageOnMainPage"
-              />
-            </div>
-            <div className={s.connectionAdaptive}>
-              <p className={s.question}>Первый вопрос бесплатно!</p>
-              <a
-                className={s.btnInstagram}
-                href="https://instagram.com/taroenes?utm_medium=copy_link"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className={s.iconInstagramWrapper}>
-                  <img
-                    className={s.iconInstagram}
-                    src={iconInstagram}
-                    alt="instagram"
-                  />
-                </div>
-                Instagram
-              </a>
-            </div>
-          </div>
-
-          <div className={s.content}>
-            <h2 className={s.title}>Гадание на картах Таро</h2>
-            <p className={s.mainText}>
-              Таинственный Оракул, актуальный несколько веков подряд – манящее и
-              загадочное Таро. Как любопытно и волнующе прикоснуться к
-              неизведанному и познать истину! Величественные мудрые Старшие
-              Арканы, детальные придворные Младшие Арканы. Они расскажут и
-              покажут вам пути к лучшей жизни, выходы из проблемных ситуаций и
-              вернут веру в себя. Что есть Таро? Уникальная, проверенная
-              временем система, открывающая вам варианты будущих событий и
-              тонкости и нюансы текущих состояний. Предсказывают ли карты Таро
-              будущее в привычном понимании? За мою многолетнюю активную
-              практику я укрепилась в знании, что Вселенная многомерна, а
-              будущее вариативно. Поэтому одного единственного будущего просто
-              не существует. Вариаций будущих событий – десятки. Они формируются
-              ежедневно, даже в эту секунду вы вершите свое будущее – не только
-              действиями, но и мыслями, намерениями, идеями. И кто знает, как
-              события вчерашнего дня откликнутся в вашей жизни через год!
-              Поэтому расклады на Таро покажут вам то, что случится с наибольшей
-              вероятностью. Вам не нравится то, что выдали карты? Тогда просто
-              спросите их, что делать, чтобы изменить ход событий в вашу пользу,
-              и таро ответят. Таро – не грозный и бескомпромиссный вершитель
-              судеб, а вековой пилигрим, указующий верный путь и предупреждающий
-              об опасностях.
-            </p>
-            <div className={s.connection}>
-              <p className={s.question}>Первый вопрос бесплатно!</p>
-              <a
-                className={s.btnInstagram}
-                href="https://instagram.com/taroenes?utm_medium=copy_link"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className={s.iconInstagramWrapper}>
-                  <img
-                    className={s.iconInstagram}
-                    src={iconInstagram}
-                    alt="instagram"
-                  />
-                </div>
-                Instagram
-              </a>
-            </div>
-          </div> */}
           </div>
         </Container>
       </section>
